a80606e02d2522c04713ec337bfeef3f536735de
Carolista <cjones.renaissancewoman@gmail.com>
2024-01-03 11:49:08 -0500

    added Shortcuts

---
+++ JavaPractice/src/main/java/gettingstarted/Shortcuts.java
@@ -0,0 +1,21 @@
+package gettingstarted;
+
+public class Shortcuts {
+
+    /*
+        Any Java class which is meant to execute code linearly must have a main method.
+        All code to be executed should be placed *inside* the main method. It has a specific
+        signature, but you can use the shortcut 'psvm' in IntelliJ to create it quickly
+
+        To execute code in IntelliJ, click on the green arrow to the left of the main
+        method signature and then click on "Run `Shortcuts.main()`"
+     */
+
+    // TODO: Use the 'psvm' shortcut with the Tab key to create a main method quickly
+
+
+    // TODO: Inside your new main method, use the shortcut 'sout' to create a statement
+    //  to print to the console. Print any String or number. Execute the main method to
+    //  to see the result in the console.
+
+}
