fe1ff7991091de1714d4a3f2bf0e2841a5fc80a1
Carolista <cjones.renaissancewoman@gmail.com>
2024-01-03 23:48:24 -0500

    added StringsAndCharacters

---
+++ JavaPractice/src/main/java/datatypesandvariables/StringsAndCharacters.java
@@ -0,0 +1,60 @@
+package datatypesandvariables;
+
+public class StringsAndCharacters {
+
+    public static void main(String[] args) {
+
+        /*
+            Declare a string variable with the type String
+            Strings must be placed between two double quotes
+         */
+
+        // TODO: Declare two strings for the first and last names of one of your favorite authors
+
+        // TODO: Print each name separately
+
+        // TODO: Print the names concatenated with a space between them
+
+
+        /*
+            Declare a character variable with the type char
+            Characters must be placed between two single quotes
+         */
+
+        // TODO: Declare characters for the initials of a favorite actor
+
+        // TODO: Print each character separately
+
+        // TODO: Print the initials concatenated with nothing between them
+
+
+        /*
+            Practice using various string methods
+         */
+
+        String album = "The Dark Side of the Moon";
+
+        // TODO: Use a string method to print the character at index 9
+
+
+        // TODO: Use a string method to print the word "Dark" from the string above
+
+
+        // TODO: Use a string method to print the index of the character 'M'
+
+
+        // TODO: Use a string method to print the string above in allcaps
+
+
+        // TODO: Print the length of the string
+
+
+        // TODO: Print true or false by using a string method to determine if the word "Light"
+        //  exists as a substring in the string above
+
+
+        // TODO: Print true or false by comparing the equality of the string "Dark Side of the
+        //  Moon" to the string above
+
+    }
+}
