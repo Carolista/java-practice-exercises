417f331f0b2a8c95582fc9d6ee688a3aeaee2a9a
Carolista <cjones.renaissancewoman@gmail.com>
2024-01-04 11:27:47 -0500

    added UsingScanner

---
+++ JavaPractice/src/main/java/userinput/UsingScanner.java
@@ -0,0 +1,39 @@
+package userinput;
+
+// TODO: Import the Scanner class from java.util
+
+/*
+    The Scanner class is used for reading data. When accepting user input, you must create
+    a new object of the Scanner class with the argument System.in.
+
+    Make sure you print something to the console first to tell the user what you're asking for,
+    then create a variable to store the input you accept.
+
+    Use Scanner class methods like .nextLine() for strings or .nextInt() for int, etc.
+
+    At the very end, always use .close() to end the process.
+ */
+
+public class UsingScanner {
+
+    public static void main(String[] args) {
+
+        // TODO: Create a Scanner object called input
+
+        // TODO: Ask the user for several pieces of information, storing each in a variable
+
+        // Their name (String)
+
+        // Their age (int)
+
+        // Their favorite cake/frosting combo (String)
+
+        // Their favorite ice cream flavor (String)
+
+        // TODO: Print a sentence or two that starts with "Happy Birthday" and uses all
+        //  information gathered from the user
+
+        // TODO: Close the scanner
+    }
+
+}
