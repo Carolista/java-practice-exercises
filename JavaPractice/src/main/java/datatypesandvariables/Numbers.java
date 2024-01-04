867fa4ef9842b40df685edd9682aa0f5cbd2f32d
Carolista <cjones.renaissancewoman@gmail.com>
2024-01-04 00:14:35 -0500

    added Numbers

---
+++ JavaPractice/src/main/java/datatypesandvariables/Numbers.java
@@ -0,0 +1,26 @@
+package datatypesandvariables;
+
+public class Numbers {
+
+    public static void main(String[] args) {
+
+        /*
+            Java has multiple data types for numbers:
+            int for whole numbers
+            float for decimal numbers up to 8 places
+            double for decimal numbers up to 16 places
+         */
+
+        // TODO: Declare an integer
+
+        // TODO: Declare a float
+        //  Either follow the number with an f
+        //  OR cast it to the type by preceding it with (float)
+
+        // TODO: Declare a double
+
+        // TODO: Print each of the variables in a sentence using concatenation
+
+        // TODO: Print the sum of all three numbers
+    }
+}
